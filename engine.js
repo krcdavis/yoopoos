669ebd83f943520ebb59e11ddb3c4b79575a73f8
krcdavis <32370620+krcdavis@users.noreply.github.com>
2026-09-22 21:38:24 -0700

    Add files via upload

---
+++ engine.js
@@ -0,0 +1,68 @@
+///draft
+
+
+function grepBeans() {
+ var beans = document.forms[0];
+ var time = 10;//defualt
+ for (const bean of beans) {
+  if (bean.checked) return bean.value;
+}
+return time;
+
+}
+
+
+  var tag = document.createElement('script');
+  tag.id = 'iframe-demo';
+  tag.src = 'https://www.youtube.com/iframe_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  function onYouTubeIframeAPIReady() {
+
+    player = new YT.Player('videobox', {
+          height: '540',
+          width: '960',
+          videoId: "sCvszgwu10w",//this is a placeholder sCvszgwu10w
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }//ev
+    });//videobox
+  }//func
+
+  function onPlayerReady(ebent) {
+    player.setVolume(50)
+    nextVid()
+  }
+
+  function onPlayerStateChange(ebent) {
+    if (ebent.data == YT.PlayerState.ENDED) {
+        nextVid()
+    }
+  }
+
+
+function nextVid() {
+ var rand = something(eps.length);
+ loadVid( eps[rand], grepBeans() );
+}
+
+
+function loadVid(vid, time) {
+var start = something(vid[1]-time);
+var end = start + time;
+
+player.loadVideoById({'videoId': vid[0],
+               'startSeconds': start,
+               'endSeconds': end});
+}//loadvid
+
+
+function something( max ) {
+ return Math.floor(Math.random() * max);
+}
